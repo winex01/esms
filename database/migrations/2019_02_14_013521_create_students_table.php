0e4758656a25a56f3cef6c71a340e0bfc2e32ff4
winnie <winnie131212592@gmail.com>
2019-02-14 23:31:47 +0800

    rename table column in student

--- database/migrations/2019_02_14_013521_create_students_table.php
+++ database/migrations/2019_02_14_013521_create_students_table.php
@@ -15,7 +15,7 @@ class CreateStudentsTable extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('student_identification');
+            $table->string('student_no');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
