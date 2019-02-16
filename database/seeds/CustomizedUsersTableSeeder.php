0aca1c4e689830af142065856aa6752a10177684
winnie <winnie131212592@gmail.com>
2019-02-16 15:57:26 +0800

    add department bread seed

--- database/seeds/CustomizedUsersTableSeeder.php
+++ database/seeds/CustomizedUsersTableSeeder.php
@@ -24,10 +24,24 @@ class CustomizedUsersTableSeeder extends Seeder
                 'avatar' => 'users/default.png',
                 'email_verified_at' => NULL,
                 'password' => '$2y$10$63.pZ1z.tiUdacyvPhzBteTBPsXpq2mdFqCK6yR4Z8svCsNvX2WzG',
-                'remember_token' => 'viQmSgaE5traA1CY65lQIMLEYFqYMNG4d2p05K4CUTPJEWg5bUausMQILgc6',
+                'remember_token' => 'QOl4DMCEmmd6UfzSnyzgnS8NzG8zpDUa6LVIeSccBQyQwdMYill4dFSQpVOz',
                 'settings' => NULL,
                 'created_at' => '2019-02-14 15:57:09',
                 'updated_at' => '2019-02-14 15:57:09',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'role_id' => 2,
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'avatar' => 'users/default.png',
+                'email_verified_at' => NULL,
+                'password' => '$2y$10$ttzVdKeyGzPnCmIRKxWHB.h2sbLis6IKzOdNF0nOLo.yX2FpEtI6m',
+                'remember_token' => 'TXRKvEMF0wwUs1L4ooFwfCkEYSfkjnDrQO9RXG9F6RXoDkgmmxF8jB3VavN0',
+                'settings' => '{"locale":"en"}',
+                'created_at' => '2019-02-15 18:12:33',
+                'updated_at' => '2019-02-15 18:12:33',
             ),
         ));
         
